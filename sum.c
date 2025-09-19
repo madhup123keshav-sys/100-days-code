4b6c23496ce8cbf21f1a021d2364711f5b0b5fc8
madhup123keshav-sys <madhup123keshav@gmail.com>
2025-09-19 21:30:55 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// write a program to add two integers and display the sum.
+
+int main(){
+    int a, b;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    printf("Sum: %d\n", a + b);
+
+    return 0;
+
+}
