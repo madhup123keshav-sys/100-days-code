310fc8d10fff8cec8a937c71c04f1e37832debf6
madhup123keshav-sys <madhup123keshav@gmail.com>
2025-09-19 23:05:29 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//write a program to swap two numbers using a third variable.
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter first number (a): ");
+    scanf("%d", &a);
+
+    printf("Enter second number (b): ");
+    scanf("%d", &b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
