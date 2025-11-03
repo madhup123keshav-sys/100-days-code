80653b6d7650a77d34a234eef6e9805c0ca7c4c5
madhup123keshav-sys <madhup123keshav@gmail.com>
2025-11-03 16:40:08 +0000

    my code

--- Day12_A_fine.c
+++ Day12_A_fine.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-Write a program to calculate library fine based on late days as follows: First 5 days late: ₹2/day Next 5 days late: ₹4/day Next 20 days days late: ₹6/day More than 30 days: Membership Cancelled.
+//write a program to calculate library fine based on late days as follows: First 5 days late: ₹2/day Next 5 days late: ₹4/day Next 20 days days late: ₹6/day More than 30 days: Membership Cancelled.
 
 
 
