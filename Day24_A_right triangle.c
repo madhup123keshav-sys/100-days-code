05b0669a52f86cb0576ecbbda1667f8e7ad37f79
madhup123keshav-sys <madhup123keshav@gmail.com>
2025-11-03 17:43:49 +0000

    my code

---
+++ Day24_A_right triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//Write a program to print the following pattern: * ** *** **** *****
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {       
+        for (int j = 1; j <= i; j++) {    
+            printf("*");
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
