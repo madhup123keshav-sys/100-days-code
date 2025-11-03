a1f6f86553fcf5ce30aa6832b09c0bdb99fb6ae7
madhup123keshav-sys <madhup123keshav@gmail.com>
2025-11-03 17:35:38 +0000

    my code

---
+++ Day22_A_check strong.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+//Write a program to check if a number is a strong number.
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num, temp, digit, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Please enter a positive integer.\n");
+        return 1;
+    }
+
+    temp = num;
+
+
+    while (temp != 0) {
+        digit = temp % 10;
+        sum += factorial(digit);
+        temp /= 10;
+    }
+
+    if (sum == num) {
+        printf("%d is a strong number.\n", num);
+    } else {
+        printf("%d is not a strong number.\n", num);
+    }
+
+    return 0;
+}
