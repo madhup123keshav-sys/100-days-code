c62f81159ebc3a35d786e62af90caa92ffb493a3
madhup123keshav-sys <madhup123keshav@gmail.com>
2025-11-03 17:41:50 +0000

    my code

---
+++ Day23_B_5*5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//Write a program to print the following pattern: ***** ***** ***** ***** *****
+
+int main() {
+    int rows = 5, cols = 5;
+
+    for (int i = 1; i <= rows; i++) {     
+        for (int j = 1; j <= cols; j++) {   
+            printf("*");
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
