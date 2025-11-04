cbdcfc2c5efb3fd12eaa51418bc114e3e9509559
madhup123keshav-sys <madhup123keshav@gmail.com>
2025-11-04 17:46:24 +0000

    my code

---
+++ Day36_B_sum of matrix.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+//Find the sum of all elements in a matrix.
+
+#define MAX_ROWS 10
+#define MAX_COLS 10
+
+int main() {
+    int matrix[MAX_ROWS][MAX_COLS];
+    int rows, cols, i, j;
+
+    printf("Enter the number of rows (1 to %d): ", MAX_ROWS);
+    if (scanf("%d", &rows) != 1 || rows <= 0 || rows > MAX_ROWS) {
+        printf("Invalid number of rows.\n");
+        return 1;
+    }
+
+    printf("Enter the number of columns (1 to %d): ", MAX_COLS);
+    if (scanf("%d", &cols) != 1 || cols <= 0 || cols > MAX_COLS) {
+        printf("Invalid number of columns.\n");
+        return 1;
+    }
+
+    printf("\nEnter %d integers to fill the matrix:\n", rows * cols);
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("Enter element [%d][%d]: ", i, j);
+            if (scanf("%d", &matrix[i][j]) != 1) {
+                printf("Invalid input.\a\n");
+                return 1;
+            }
+        }
+    }
+
+    printf("\n--- The matrix you entered is: ---\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+           
+            printf("%-4d ", matrix[i][j]);
+        }
+        printf("\n"); 
+    }
+
+    return 0; 
+}
