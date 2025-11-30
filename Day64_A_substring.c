9d56e8d9aea4a9ef75283156b39d380b1af4f14f
madhup123keshav-sys <madhup123keshav@gmail.com>
2025-11-30 08:11:42 +0000

    my code

---
+++ Day64_A_substring.c
@@ -0,0 +1,34 @@
+// Q114: Write a program to take a string s as input.
+// The task is to find the length of the longest substring without repeating characters. Print the length as output.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    printf("Enter a string: ");
+    scanf("%s", s);
+    
+    int n = strlen(s);
+    int maxLen = 0, start = 0;
+    int lastIndex[256];
+    
+    
+    for (int i = 0; i < 256; i++)
+        lastIndex[i] = -1;
+    
+    for (int end = 0; end < n; end++) {
+        
+        if (lastIndex[(unsigned char)s[end]] >= start)
+            start = lastIndex[(unsigned char)s[end]] + 1;
+        
+        lastIndex[(unsigned char)s[end]] = end;
+        
+        int currentLen = end - start + 1;
+        if (currentLen > maxLen)
+            maxLen = currentLen;
+    }
+    
+    printf("%d\n", maxLen);
+    return 0;
+}
