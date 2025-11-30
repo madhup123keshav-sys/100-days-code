d7653a47890bd976c5c15afda9026983554b4726
madhup123keshav-sys <madhup123keshav@gmail.com>
2025-11-30 08:13:52 +0000

    my code

---
+++ Day66_A_logicen.c
@@ -0,0 +1,42 @@
+//Q116 (Logic Enhancers)Write a program to take an integer array nums which contains only positive integers, and an integer target as inputs. The goal is to find two distinct indices i and j in the array such that nums[i] + nums[j] equals the target. Assume exactly one solution exists and return the indices in any order. Print the two indices separated by a space as output. If no solution exists, print "-1 -1".
+// Q116 (Logic Enhancers)
+// Find two indices in an array such that their sum equals the target
+
+#include <stdio.h>
+
+int main() {
+    int n, target;
+    int nums[100];
+    int i, j, found = 0;
+
+    
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+
+    printf("Enter array elements: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    printf("Enter target value: ");
+    scanf("%d", &target);
+
+    
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                printf("%d %d\n", i, j);
+                found = 1;
+                break;
+            }
+        }
+        if (found) break;
+    }
+
+    if (!found) {
+        printf("-1 -1\n");
+    }
+
+    return 0;
+}
